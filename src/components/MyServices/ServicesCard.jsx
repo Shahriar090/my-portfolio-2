a62024b51c1a4f22a1b3d11c4a7ac81ba32e2820
Shahriar <shahriarh655@gmail.com>
2024-04-08 03:52:25 +0600

    fixed issue about responsiveness from services card

--- src/components/MyServices/ServicesCard.jsx
+++ src/components/MyServices/ServicesCard.jsx
@@ -4,19 +4,19 @@ const ServicesCard = ({ data }) => {
   const { service, description, serviceImg } = data;
   return (
     <div className="w-full bg-[#222222]  rounded-md border border-[#333] cursor-pointer hover:border-[#FF5D56] transition-all duration-500">
-      <div className="service-container w-full h-full p-8">
-        <div className="img h-20 w-20 mx-auto">
+      <div className="service-container w-full h-full p-0 sm:p-8">
+        <div className="img h-20 w-20 mx-auto pt-5 sm:pt-0">
           <img
             src={serviceImg}
             alt="Service Image"
             className=" bg-[#FF5D56] p-3 rounded-md"
           />
         </div>
-        <div className="details text-center mt-6">
+        <div className="details text-center mt-12 sm:mt-6">
           <h2 className="text-[#F1FFFF] text-xl font-bold capitalize">
             {service}
           </h2>
-          <p className="text-[#707070] text-[16px] pt-6 text-center max-w-sm mx-auto">
+          <p className="text-[#707070] text-[16px] py-5 sm:pt-6 text-center max-w-sm mx-auto">
             {description}
           </p>
         </div>
