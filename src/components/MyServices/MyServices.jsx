6e697fe2a32bcb0f8021f89a7cd7350e86d6e947
Shahriar <shahriarh655@gmail.com>
2024-04-08 03:59:40 +0600

    fixing issues

--- src/components/MyServices/MyServices.jsx
+++ src/components/MyServices/MyServices.jsx
@@ -46,7 +46,7 @@ const MyServices = () => {
 
         {/* main content start */}
         <section>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
             {services.map((service) => (
               <ServicesCard key={service.id} data={service} />
             ))}
