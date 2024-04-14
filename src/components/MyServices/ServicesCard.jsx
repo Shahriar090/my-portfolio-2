ad1ff7233c52d176f353a95cbd3fafeb1cf6d342
Shahriar <shahriarh655@gmail.com>
2024-04-15 00:38:02 +0600

    Fixed minor issue from services card component

--- src/components/MyServices/ServicesCard.jsx
+++ src/components/MyServices/ServicesCard.jsx
@@ -3,20 +3,20 @@ import PropTypes from "prop-types";
 const ServicesCard = ({ data }) => {
   const { service, description, serviceImg } = data;
   return (
-    <div className="w-full bg-[#222222]  rounded-md border border-[#333] cursor-pointer hover:border-[#FF5D56] transition-all duration-500 p-3">
+    <div className="w-full bg-[#222222]  rounded-md border border-[#333] cursor-pointer hover:border-[#FF5D56] transition-all duration-500 p-3 sm:p-5">
       <div className="service-container">
-        <div className="img h-14 w-14">
+        <div className="img h-16 w-16 mx-auto">
           <img
             src={serviceImg}
             alt="Service Image"
             className=" bg-[#FF5D56] p-3 rounded-md"
           />
         </div>
-        <div className="details text-start">
+        <div className="details text-center">
           <h2 className="text-white text-xl font-semibold capitalize py-3">
             {service}
           </h2>
-          <p className="text-[#707070] text-lg  text-start">{description}</p>
+          <p className="text-[#707070] text-lg  text-center">{description}</p>
         </div>
       </div>
     </div>
