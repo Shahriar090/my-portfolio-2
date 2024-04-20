87df060c5d7cce590a610367ed2818d2a369adda
Shahriar <shahriarh655@gmail.com>
2024-04-20 22:26:57 +0600

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AboutMe from "./components/About/AboutMe";
 import HeroBanner from "./components/Banner/HeroBanner";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import MyServices from "./components/MyServices/MyServices";
 import NavBar from "./components/NavBar/NavBar";
 import Projects from "./components/Projects/Projects";
@@ -18,6 +19,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import Container from "../../shared/Container";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <section className="bg-[#111111]">
+      <Container>
+        <footer>
+          <aside className="flex justify-center">
+            <div className="flex flex-col text-center text-[#707070] text-lg">
+              <p>Copyright &copy; {currentYear}</p>
+              <p>All Rights Reserved By Mohammad Shahriar Hossain</p>
+            </div>
+          </aside>
+        </footer>
+      </Container>
+    </section>
+  );
+};
+
+export default Footer;
