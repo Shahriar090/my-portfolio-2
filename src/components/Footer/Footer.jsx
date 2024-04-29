ef1fb4f8dc32086f54a5b082630294cedc6a7c79
Shahriar <shahriarh655@gmail.com>
2024-04-29 22:21:01 +0600

    Fixed some minor issues from footer and services component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import Container from "../../shared/Container";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <section className="bg-[#111111] border-t-2 border-[#333]">
+    <section className="bg-[#111111] border-t-4 border-[#333]">
       <Container>
         <footer>
           <aside className="flex justify-center">
             <div className="flex flex-col text-center text-[#707070] text-lg">
-              <p>Copyright &copy; {currentYear}</p>
+              <p>&copy; {currentYear}</p>
               <p>All Rights Reserved By Mohammad Shahriar Hossain</p>
             </div>
           </aside>
--- src/components/MyServices/ServicesCard.jsx
+++ src/components/MyServices/ServicesCard.jsx
@@ -5,14 +5,14 @@ const ServicesCard = ({ data }) => {
   return (
     <div className="w-full h-full bg-[#222222]  rounded-sm border border-[#333] cursor-pointer hover:border-[#FF5D56] transition-all duration-500 py-10 px-7">
       <div className="service-container flex flex-col">
-        <div className="img img flex h-16 w-16 mx-auto">
+        <div className="img img flex h-14 w-14">
           <img
             src={serviceImg}
             alt="Service Image"
             className=" bg-[#FF5D56] p-3 rounded-md"
           />
         </div>
-        <div className="details text-center">
+        <div className="details text-start">
           <h2 className="text-white text-xl font-medium capitalize py-3">
             {service}
           </h2>
